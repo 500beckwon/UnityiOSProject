d6d5ae548c0055bdbcd364037f63d1b5d503db33
500beckwon <kjsx60@gmai.com>
2022-07-20 19:20:36 +0900

    Plugin Objective-C Code

---
+++ iOSPluginProject/Assets/Plugin/PluginTest.h
@@ -0,0 +1,13 @@
+//
+//  PluginTest.h
+//  UnityiOSPlugin
+//
+//  Created by ByungHoon Ann on 2022/06/21.
+//
+
+#ifndef PluginTest_h
+#define PluginTest_h
+
+#import <Foundation/Foundation.h>
+
+#endif /* PluginTest_h */
---
+++ iOSPluginProject/Assets/Plugin/PluginTest.mm
@@ -0,0 +1,12 @@
+//
+//  PluginTest.m
+//  UnityiOSPlugin
+//
+//  Created by ByungHoon Ann on 2022/06/21.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <UnityiOSPlugin/UnityiOSPlugin.h>
+#import "UnityiOSPlugin/UnityiOSPlugin-Swift.h"
+#import "UnityiOSPlugin.h"
