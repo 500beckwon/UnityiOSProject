2526a3fae15e5f7ac4836cbfaba1ce0c9658f5d1
500beckwon <kjsx60@gmai.com>
2022-07-20 19:20:17 +0900

    iOS Framework

---
+++ iOSPluginProject/Assets/Plugin/UnityiOSPlugin.framework/Headers/UnityiOSPlugin.h
@@ -0,0 +1,18 @@
+//
+//  UnityiOSPlugin.h
+//  UnityiOSPlugin
+//
+//  Created by ByungHoon Ann on 2022/06/11.
+//
+
+#import <Foundation/Foundation.h>
+#import <UnityiOSPlugin/UnityiOSPlugin.h>
+//! Project version number for UnityiOSPlugin.
+FOUNDATION_EXPORT double UnityiOSPluginVersionNumber;
+
+//! Project version string for UnityiOSPlugin.
+FOUNDATION_EXPORT const unsigned char UnityiOSPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnityiOSPlugin/PublicHeader.h>
+
+
